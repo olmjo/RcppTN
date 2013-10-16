14f5235364047d6457b22d05b080c075efc91511
olmjo <jpolmsted@gmail.com>
2013-10-16 09:37:10 -0400

    switch to registration

--- pkg/R/rtn.R
+++ pkg/R/rtn.R
@@ -3,8 +3,7 @@ rtn <- function(.mean = rep(0, 1),
                 .low = rep(-Inf, length(.mean)),
                 .high = rep(Inf, length(.mean))
                 ) {
-    out <- .Call("rtnRcpp",
-                 PACKAGE = "RcppTN",
+    out <- .Call(rtnRcpp,
                  mean = .mean,
                  sd = .sd,
                  low = .low,
