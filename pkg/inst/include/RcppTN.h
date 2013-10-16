af8cbd4c29a54ef118abd858a6245e1068de9640
olmjo <jpolmsted@gmail.com>
2013-10-16 10:44:52 -0400

    RcppTN API

--- pkg/inst/include/RcppTN.h
+++ pkg/inst/include/RcppTN.h
@@ -1,1 +1,9 @@
-#include "rtn1.h"
+#include <R_ext/Rdynload.h>
+
+namespace RcppTN {
+    inline double rtn1(double mean, double sd, double low, double high){
+        typedef double(*Fun)(double, double, double, double) ;
+        static Fun rtn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_rtn1") ;
+        return rtn1_(mean, sd, low, high) ;
+    }
+}
