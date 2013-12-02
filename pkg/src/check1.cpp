b8124b33f6ae81fdec0adb949315c6b51dcf6d84
Jonathan Paul Olmsted <jpolmsted@gmail.com>
2013-12-02 14:39:29 -0500

    add check for finite mean

--- pkg/src/check1.cpp
+++ pkg/src/check1.cpp
@@ -9,6 +9,12 @@ bool check1(const double mean,
   if (sd <= 0) {
     flag = false ;
   }
+  if (mean == R_NegInf) {
+    flag = false ;    
+  }
+  if (mean == R_PosInf) {
+    flag = false ;
+  }
   if (low >= high) {
     flag = false ;
   }
