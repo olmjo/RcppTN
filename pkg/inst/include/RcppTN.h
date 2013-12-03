27b3e5fd25a5851417db5c33672363e1e7c5c327
Jonathan Paul Olmsted <jpolmsted@gmail.com>
2013-12-03 12:31:30 -0500

    add ent() to API

--- pkg/inst/include/RcppTN.h
+++ pkg/inst/include/RcppTN.h
@@ -1,9 +1,15 @@
 #include <R_ext/Rdynload.h>
 
 namespace RcppTN {
-    inline double rtn1(double mean, double sd, double low, double high){
-        typedef double(*Fun)(double, double, double, double) ;
-        static Fun rtn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_rtn1") ;
-        return rtn1_(mean, sd, low, high) ;
-    }
+  typedef double(*Fun)(double, double, double, double) ;
+
+  inline double rtn1(double mean, double sd, double low, double high){
+    static Fun rtn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_rtn1") ;
+    return rtn1_(mean, sd, low, high) ;
+  }
+
+  inline double etn1(double mean, double sd, double low, double high){
+    static Fun etn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_etn1") ;
+    return etn1_(mean, sd, low, high) ;
+  }
 }
