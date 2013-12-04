0773c8582eaf40b45736a932fe0583c2865bd066
Jonathan Paul Olmsted <jpolmsted@gmail.com>
2013-12-04 18:57:01 -0500

    expanded memleak testing

--- debug/run.sh
+++ debug/run.sh
@@ -2,3 +2,6 @@ R -d "valgrind --dsymutil=yes --tool=memcheck \
       --leak-check=full --track-origins=yes \
       --log-file=./${1}.log" \
       --vanilla < ${1}
+
+echo -e '\n\n\n' >> ${1}.log
+echo `date` >> ${1}.log
--- debug/treat1.R
+++ debug/treat1.R
@@ -5,40 +5,44 @@ gctorture(TRUE)
 N <- 20
 MC <- 20
 
-for(i in 1:MC) {
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(-1, N),
-        .high = rep(1, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(5, N),
-        .high = rep(Inf, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(-Inf, N),
-        .high = rep(10, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(4, N),
-        .high = rep(7, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(4, N),
-        .high = rep(4.1, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(-Inf, N),
-        .high = rep(Inf, N)
-        )
-    rtn(.mean = rep(0, N),
-        .sd = rep(1, N),
-        .low = rep(50, N),
-        .high = rep(100, N)
-        )
+funcs <- c("rtn", "etn", "vtn")
+
+for (f in funcs) {
+    for (i in 1:MC) {
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(-1, N),
+               .high = rep(1, N)
+               )
+        get(f)(.mean = rep(0, N),
+            .sd = rep(1, N),
+            .low = rep(5, N),
+            .high = rep(Inf, N)
+            )
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(-Inf, N),
+               .high = rep(10, N)
+               )
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(4, N),
+               .high = rep(7, N)
+               )
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(4, N),
+               .high = rep(4.1, N)
+               )
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(-Inf, N),
+               .high = rep(Inf, N)
+               )
+        get(f)(.mean = rep(0, N),
+               .sd = rep(1, N),
+               .low = rep(5, N),
+               .high = rep(100, N)
+               )
+    }
 }
