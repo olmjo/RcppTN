ae1ea77340d1cc06b32cf5b79cb7e9e04e4fc06e
olmjo <jpolmsted@gmail.com>
2014-03-28 15:41:42 -0400

    whitespace stuff

--- pkg/inst/include/RcppTN.h
+++ pkg/inst/include/RcppTN.h
@@ -4,12 +4,12 @@
 #include <R_ext/Rdynload.h>
 
 namespace RcppTN {
-  typedef double(*Fun)(double, double, double, double) ;
+    typedef double(*Fun)(double, double, double, double) ;
 
-  inline double rtn1(double mean, double sd, double low, double high){
-    static Fun rtn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_rtn1") ;
+    inline double rtn1(double mean, double sd, double low, double high){
+        static Fun rtn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_rtn1") ;
     return rtn1_(mean, sd, low, high) ;
-  }
+    }
 
   inline double etn1(double mean, double sd, double low, double high){
     static Fun etn1_ = (Fun)R_GetCCallable("RcppTN", "RcppTN_etn1") ;
--- pkg/src/RcppTN_init.cpp
+++ pkg/src/RcppTN_init.cpp
@@ -6,7 +6,7 @@
 #include "vtn1.h"
 
 RcppExport void R_init_RcppTN (DllInfo *info) {
-R_RegisterCCallable("RcppTN", "RcppTN_etn1", (DL_FUNC) etn1) ;
-R_RegisterCCallable("RcppTN", "RcppTN_rtn1", (DL_FUNC) rtn1) ;
-R_RegisterCCallable("RcppTN", "RcppTN_vtn1", (DL_FUNC) vtn1) ;
+  R_RegisterCCallable("RcppTN", "RcppTN_etn1", (DL_FUNC) etn1) ;
+  R_RegisterCCallable("RcppTN", "RcppTN_rtn1", (DL_FUNC) rtn1) ;
+  R_RegisterCCallable("RcppTN", "RcppTN_vtn1", (DL_FUNC) vtn1) ;
 }
