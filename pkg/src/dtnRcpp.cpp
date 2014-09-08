9970986caff3bcbe102cb8ed1eb4eb5c1dff2431
olmjo <jpolmsted@gmail.com>
2014-09-08 12:34:53 -0400

    fix indentation

--- pkg/src/dtnRcpp.cpp
+++ pkg/src/dtnRcpp.cpp
@@ -7,28 +7,28 @@
 
 RcppExport SEXP dtnRcpp(const SEXP x,
                         const SEXP mean,
-			const SEXP sd,
-			const SEXP low,
-			const SEXP high
-			) {
-
+                        const SEXP sd,
+                        const SEXP low,
+                        const SEXP high
+                        ) {
+    
   // Namespace
-  using namespace Rcpp ;
-  //
-  
-  // Conversion of Inputs
-  const NumericVector X(x) ;
-  const NumericVector Mean(mean) ;
-  const NumericVector Sd(sd) ;
-  const NumericVector Low(low) ;
-  const NumericVector High(high) ;
-  //
-  
-  // Init, Populate, and Return
-  NumericVector Dens(X.size(), 0.0) ;
-  {
-    dtn(X, Mean, Sd, Low, High, Dens) ;
-  }
-  return(Dens) ;
-  //
+    using namespace Rcpp ;
+    //
+    
+    // Conversion of Inputs
+    const NumericVector X(x) ;
+    const NumericVector Mean(mean) ;
+    const NumericVector Sd(sd) ;
+    const NumericVector Low(low) ;
+    const NumericVector High(high) ;
+    //
+    
+    // Init, Populate, and Return
+    NumericVector Dens(X.size(), 0.0) ;
+    {
+        dtn(X, Mean, Sd, Low, High, Dens) ;
+    }
+    return(Dens) ;
+    //
 }
