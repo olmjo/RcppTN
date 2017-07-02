b748d59e562a7b8b7aacd9a62a9773e87121dcc7
Jonathan Olmsted <jonathan.olmsted@yale.edu>
2017-07-02 05:32:33 -0400

    add new calls for R->Cpp bridge
    Without this, it compiled under 3.4.0, but generated a CRAN check warning

--- src/RcppTN_init.cpp
+++ src/RcppTN_init.cpp
@@ -15,4 +15,7 @@ RcppExport void R_init_RcppTN (DllInfo *info) {
     R_RegisterCCallable("RcppTN", "RcppTN_vtn1", (DL_FUNC) vtn1) ;
     R_RegisterCCallable("RcppTN", "RcppTN_dtn1", (DL_FUNC) dtn1) ;
     R_RegisterCCallable("RcppTN", "RcppTN_enttn1", (DL_FUNC) enttn1) ;
+    R_registerRoutines(info, NULL, NULL, NULL, NULL) ;
+    R_useDynamicSymbols(info, TRUE) ;
+
 }
